4f01ca94bd16e6b48ea2b9d1336f657515763c63
mw4rf <mw4rf@sent.at>
2024-05-30 16:23:21 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module edax
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+// Main
+func main() {
+	// Get command line arguments
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("Usage: edax [command] <value>")
+		fmt.Println("Commands:")
+		fmt.Println("  version 			=> Show version number")
+		fmt.Println("  create <name> 		=> Create a new timer with a name")
+		fmt.Println("  start <id> 			=> Start a timer")
+		fmt.Println("  reset <id> 			=> Reset a timer")
+		fmt.Println("  stop <id> 			=> Stop a timer")
+		fmt.Println("  list 				=> List all timers")
+		os.Exit(1)
+	}
+
+	// Load the timers from JSON file
+	timers := loadTimers()
+	defer saveTimers(&timers)
+
+	// Switch the command
+	switch args[0] {
+	case "version":
+		fmt.Println("Edax v0.2")
+	case "create":
+		t := createTimer(len(timers), args[1])
+		timers = append(timers, t)
+	case "start":
+		if len(args) < 2 {
+			fmt.Println("Usage: edax start <id>")
+			os.Exit(1)
+		}
+		id := args[1]
+		startTimer(&timers, parseInt(id))
+	case "reset":
+		if len(args) < 2 {
+			fmt.Println("Usage: edax reset <id>")
+			os.Exit(1)
+		}
+		id := args[1]
+		resetTimer(&timers, parseInt(id))
+	case "stop":
+		if len(args) < 2 {
+			fmt.Println("Usage: edax stop <id>")
+			os.Exit(1)
+		}
+		id := args[1]
+		stopTimer(&timers, parseInt(id))
+	case "list":
+		printList(&timers)
+	}
+}
+
+// Parse a string to an integer
+func parseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Error parsing integer")
+		os.Exit(1)
+	}
+	return i
+}
+
+// Timer struct
+type Timer struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Start   int64  `json:"start"`
+	End     int64  `json:"end"`
+	Running bool   `json:"running"`
+}
+
+// Create a new timer
+func createTimer(id int, name string) Timer {
+	return Timer{
+		Id:      id,
+		Name:    name,
+		Start:   0,
+		End:     0,
+		Running: false,
+	}
+}
+
+// Print the list of timers
+func printList(timers *[]Timer) {
+	const (
+		grey      = "\033[90m"
+		lightBlue = "\033[94m"
+		red       = "\033[91m"
+		green     = "\033[92m"
+		reset     = "\033[0m"
+	)
+
+	fmt.Println(grey + "Timers List:" + reset)
+	fmt.Println(grey + "------------------------------------------------------" + reset)
+	for _, timer := range *timers {
+		status, color := getStatus(timer)
+		duration := calculateDuration(timer.Start, timer.End, timer.Running)
+		fmt.Printf("[%d] %s%s%s %s%s%s\n", timer.Id, color, status, reset, lightBlue, timer.Name, reset)
+		fmt.Printf("%sStart: %-19s End: %-19s Duration: %s%s\n", grey, formatTime(timer.Start), formatTime(timer.End), duration, reset)
+		fmt.Println(grey + "------------------------------------------------------" + reset)
+	}
+}
+
+// Get the status of the timer as an emoji with appropriate color
+func getStatus(timer Timer) (string, string) {
+	const (
+		red    = "\033[91m"
+		green  = "\033[92m"
+	)
+
+	if timer.Running {
+		return "▶️ Running", green
+	} else {
+		return "⏹️ Stopped", red
+	}
+}
+
+// Format time from int64 to a readable string
+func formatTime(timestamp int64) string {
+	if timestamp == 0 {
+		return "N/A"
+	}
+	t := time.Unix(timestamp, 0)
+	return t.Format("2006-01-02 15:04:05")
+}
+
+// Calculate the duration between start and end times
+func calculateDuration(start, end int64, running bool) string {
+	if start == 0 {
+		return "Not Started"
+	}
+	if running {
+		end = time.Now().Unix()
+	}
+	duration := end - start
+	return fmt.Sprintf("%d seconds", duration)
+}
+
+// Start a timer identified by its id
+func startTimer(timers *[]Timer, id int) {
+	if id < 0 || id >= len(*timers) {
+		fmt.Println("Invalid timer ID")
+		return
+	}
+	timer := &(*timers)[id]
+	if timer.Running {
+		fmt.Println("Timer is already running")
+		return
+	}
+	timer.Start = time.Now().Unix()
+	timer.End = 0
+	timer.Running = true
+	fmt.Printf("Timer %s started at %s\n", timer.Name, formatTime(timer.Start))
+}
+
+// Reset a timer identified by its id
+func resetTimer(timers *[]Timer, id int) {
+	if id < 0 || id >= len(*timers) {
+		fmt.Println("Invalid timer ID")
+		return
+	}
+	timer := &(*timers)[id]
+	timer.Start = 0
+	timer.End = 0
+	timer.Running = false
+	fmt.Printf("Timer %s reset\n", timer.Name)
+}
+
+// Stop a timer identified by its id
+func stopTimer(timers *[]Timer, id int) {
+	if id < 0 || id >= len(*timers) {
+		fmt.Println("Invalid timer ID")
+		return
+	}
+	timer := &(*timers)[id]
+	if timer.Running {
+		timer.Running = false
+		timer.End = time.Now().Unix()
+		fmt.Printf("Timer %s stopped at %s\n", timer.Name, formatTime(timer.End))
+	} else {
+		fmt.Println("Timer is not running")
+	}
+}
+
+// Save all timers to a file as a JSON object
+func saveTimers(timers *[]Timer) {
+	file, err := os.Create("timers.json")
+	if err != nil {
+		fmt.Println("Error creating file")
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(timers)
+	if err != nil {
+		fmt.Println("Error writing JSON object")
+		return
+	}
+}
+
+// Load all timers from a file as a JSON object
+func loadTimers() []Timer {
+	file, err := os.Open("timers.json")
+	if err != nil {
+		return make([]Timer, 0)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	timers := make([]Timer, 0)
+	err = decoder.Decode(&timers)
+	if err != nil {
+		return make([]Timer, 0)
+	}
+	return timers
+}
